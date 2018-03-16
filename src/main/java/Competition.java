774662ad875325d05160030943a3ea00d95d696a
daniel kelly <danieljkelly91@gmail.com>
2018-03-16 11:33:49 +0000

    added getters and setters for competiton class

---
+++ src/main/java/Competition.java
@@ -0,0 +1,54 @@
+import java.util.GregorianCalendar;
+import java.util.Set;
+
+public abstract class Competition {
+
+    private int id;
+    private String name;
+    private Set<Team> teams;
+    private GregorianCalendar startDate;
+    private GregorianCalendar endDate;
+
+    public Competition() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(Set<Team> teams) {
+        this.teams = teams;
+    }
+
+    public GregorianCalendar getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(GregorianCalendar startDate) {
+        this.startDate = startDate;
+    }
+
+    public GregorianCalendar getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(GregorianCalendar endDate) {
+        this.endDate = endDate;
+    }
+}
